5238d8ebea190d061421452b980459d329847d4d
Divyanshu Singh <112723454+SinghDivyanshu12@users.noreply.github.com>
2026-05-05 15:53:35 +0530

    Add routing for login, signup, and dashboard pages

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Dashboard from "../pages/Dashboard";
+import Projects from "../pages/Projects";
+import Tasks from "../pages/Tasks";
+import PrivateRoute from "../utils/PrivateRoute";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
